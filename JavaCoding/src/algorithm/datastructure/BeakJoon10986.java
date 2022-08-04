8abd758ec622f39fb3f8a80bb50268e5d8c54074
DESKTOP-NGGMCVT\yousd <yousd179@gmail.com>
2022-08-05 00:12:08 +0900

    알고리즘 공부중

--- JavaCoding/src/algorithm/datastructure/BeakJoon10986.java
+++ JavaCoding/src/algorithm/datastructure/BeakJoon10986.java
@@ -2,44 +2,46 @@ package algorithm.datastructure;
 
 import java.util.Scanner;
 import java.util.StringTokenizer;
-
+import java.io.IOException;
 public class BeakJoon10986 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		/*
-		 * 슈도코드 작성하기 number 숫자의 길이 divideNum 나눗셈할 수 countNum 누적합 baseArr 원래 배열 count
-		 * 나뉜숫자 int[]countNum = 새배열[baseArr+1] StringTokenizer stz = new
-		 * StringTokenizer(sc.nextInt()) number,divide 받기 StringTokenizer로 다시 받기
-		 * for(i=1부터 i~n까지){ countNum[i] = countNum[i-1]+sc.nextInt(); } for(i=1 i~n까지){
-		 * (countNum[i]%divideNum==0)의 개수를 구하고 } for(i=count ; n이 2보다 크거나 같으면){ if(i가
-		 * 2보다 같거나 크다면) count는 count+i*(i-1)/2 }
+		 슈도코드 작성
+		 N 숫자의 개수 입력
+		 M 나눌려는 수 입력
+		 int[] countNum에 나머지0값 저장
+		 (A+B)%C == 0;
+		 A%C+A%C == 0;
+		 구간 별로 나머지가 0일경우
+		 (B-A)%C == 0;
+		 B%C - A%C == 0; B%C == A%C 인구간도 찾고
+		 nC2로 각 나머지구간의 합을 구해서 더하면된다.
 		 */
 		Scanner sc = new Scanner(System.in);
 		StringTokenizer stz = new StringTokenizer(sc.nextLine());
 		int numberLength = Integer.parseInt(stz.nextToken());
 		int divideNum = Integer.parseInt(stz.nextToken());
-		int count = 0;
+		int[] countNum = new int[divideNum];
 		stz = new StringTokenizer(sc.nextLine());
-		int[] countNum = new int[numberLength + 1];
-
-		for (int i = 1; i <= numberLength; i++) {
-			// 누적합 구해주고
-			countNum[i] = countNum[i - 1] + Integer.parseInt(stz.nextToken());
+		//누적 합과 나머지 구하기
+		int sum = 0;
+		for(int i=0; i<numberLength ; i++) {
+			sum = (sum+Integer.parseInt(stz.nextToken()))%divideNum;
+			countNum[sum]++;
+			//나머지 0부터 divideNum-1까지 
 		}
-		for (int i = 1; i <= numberLength; i++) {
-			// 나머지 0인것 고르기
-			if (countNum[i] % divideNum == 0) {
-				count++;
-			}
-		}
-		// i가 2까지
-		for (int i = count; i >= 2; i--) {
-			int addCount = i * (i - 1) / 2;// nC2
-			count += addCount;
-		}
-		System.out.println(count);
 		
+		//count = countNum[0] 나눳을경우 0인 경우
+		long count = countNum[0];
+		
+		for(int i=0 ; i<divideNum ; i++) {
+			count+=(long)countNum[i]*(countNum[i]-1)/2;
+		}
+		
+		System.out.println(count);
+
 		sc.close();
 	}
 }
